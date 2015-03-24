f5149548d56c07dc5675e84a3e4167618979eaa9
LLafodog <silvert.ervan@gmail.com>
2015-03-24 13:14:41 +0100

    Ajout de texte et de corde d'ascenceur

--- Elevator_project.java
+++ Elevator_project.java
@@ -14,10 +14,12 @@ public class Elevator_project
     final static double BORDER_GAP = 0.1 * WINDOW_SIZE ;
     final static double FLOOR_WIDTH = WINDOW_SIZE * 0.35 ;
     final static double FLOOR_HEIGHT = WINDOW_SIZE * 0.1 ;
-    final static double ELEVATOR_WIDTH = WINDOW_SIZE - 2 * FLOOR_WIDTH - 2 * BORDER_GAP  ;
+    final static double ELEVATOR_WIDTH = WINDOW_SIZE - 2 * FLOOR_WIDTH - 2 * BORDER_GAP - 2  ;
     final static double ELEVATOR_HEIGHT = FLOOR_HEIGHT  ;
     final static double PASSENGER_WIDTH = 2;
     final static double PASSENGER_HEIGHT = 2 * FLOOR_HEIGHT / 3;
+    final static int CHAR_WIDTH = 10;
+    final static int CHAR_HEIGHT = 15;
     /* ========================= */
 
 
@@ -266,12 +268,12 @@ public class Elevator_project
 	int b = 0;
     }
 
-    static Color createNewColor(int r, int g, int b)
+    static Color createNewColor(double r, double g, double b)
     {
 	Color color = new Color();
-	color.r = r;
-	color.g = g;
-	color.b = b;
+	color.r = (int)(r);
+	color.g = (int)(g);
+	color.b = (int)(b);
 	return color;
     }
 
@@ -293,7 +295,26 @@ public class Elevator_project
 		drawRectangle(left,window,createNewColor(4,139,154)); // Here is the floor color
 		drawRectangle(right,window,createNewColor(4,139,154));
 		drawPassengersInFloor(left,right,currentFloor.passengers,window);
+		
+		// Drawing the number of passengers
+		String writting = "Waiting :"+Integer.toString((int)(currentFloor.passengers));
+		drawPanel(left.x+left.width-writting.length()*CHAR_WIDTH,left.y + 1/6.0*left.height,writting,window, createNewColor(150,150,150));
+
+		// Drawing the number of the floor
+		String index = "Floor number:" + Integer.toString((int)(i));
+		drawPanel(right.x+right.width/2-writting.length()*CHAR_WIDTH/2,right.y + 1/6.0*right.height,index,window, createNewColor(4,139,154));
+
 	    }
+
+
+    }
+
+    static void drawPanel(double x, double y , String writting, EcranGraphique window, Color color)
+    {
+	window.setColor(color.r,color.g,color.b);
+	int size = writting.length();
+	window.drawText((int)(x),(int)(y), window.COLABA8x13,writting);
+	window.drawRect((int)(x-5),(int)(y-CHAR_HEIGHT),(int)(size*CHAR_WIDTH+5),CHAR_HEIGHT+2);
     }
     
     static void drawElevator(Elevator elevator, EcranGraphique window, int dt)
@@ -302,11 +323,15 @@ public class Elevator_project
 	int lastLevel = elevator.waitingList.length;
 
 	// Elevator
-	rectangle.x = BORDER_GAP + FLOOR_WIDTH;
+	rectangle.x = BORDER_GAP + FLOOR_WIDTH+1;
 	rectangle.y = (lastLevel)*FLOOR_HEIGHT - (1/3.0 * elevator.positionBy3) * FLOOR_HEIGHT - elevator.direction*dt/1000.0 * FLOOR_HEIGHT/3.0;
 	rectangle.width = ELEVATOR_WIDTH;
 	rectangle.height = ELEVATOR_HEIGHT;
 	drawRectangle(rectangle, window, createNewColor(128,0,0)); // Here is the elevator color 
+
+	// Cables
+	window.drawLine((int)(rectangle.x +0.3*rectangle.width) ,(int)(FLOOR_HEIGHT),(int)(rectangle.x + 0.3*rectangle.width), (int)(rectangle.y));
+	window.drawLine((int)(rectangle.x +0.6*rectangle.width) ,(int)(FLOOR_HEIGHT),(int)(rectangle.x + 0.6*rectangle.width), (int)(rectangle.y));
 
 	//Passengers 
 	for(short i=0;i<elevator.passengers;i++)
@@ -316,6 +341,11 @@ public class Elevator_project
 		xOffset = i / numberMaxInWidth;
 		    drawPassenger(rectangle.x+(i%numberMaxInWidth)*PASSENGER_WIDTH+xOffset,rectangle.y+rectangle.height/3.0,window,createNewColor(0,128,255));
 	    }
+	// Drawing panel 
+	String nbPass = Integer.toString((int)(elevator.passengers));
+	double ratio = (double)(elevator.passengers) / (double)(elevator.CAPACITY);
+	Ecran.afficherln("Color : " , 255.0*(1.0-ratio) );
+	drawPanel(rectangle.x + 1/3.0 * rectangle.width, rectangle.y+2,nbPass,window,createNewColor(255,255.0*(1.0-ratio),255.0*(1.0-ratio)));
 
 	
     }
